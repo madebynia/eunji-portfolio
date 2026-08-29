0d67a8086bcd656a3b701aa2fce9affff7b61403
wooju_world <48282741+madebynia@users.noreply.github.com>
2026-08-29 22:33:28 +0900

    refactor: keep shared navigation in route shell

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
 import { SiteFooter } from '../components/SiteFooter';
-import { SiteHeader } from '../components/SiteHeader';
 import '../styles/tokens.css';
 import '../styles/global.css';
 import { AppRoutes } from './routes';
@@ -9,7 +8,6 @@ export function App() {
   return (
     <BrowserRouter>
       <div className="site-frame">
-        <SiteHeader />
         <AppRoutes />
         <SiteFooter />
       </div>
